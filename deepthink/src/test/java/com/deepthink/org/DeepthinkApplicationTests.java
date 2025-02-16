942b0b4b9db37bd5e7f50f26a39a3675e8c212c8
Neeraj Kumar <neerajkumarroy805@gmail.com>
2025-02-16 23:56:52 +0530

    Test Commit.

---
+++ deepthink/src/test/java/com/deepthink/org/DeepthinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepthink.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepthinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
